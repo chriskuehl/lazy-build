9f237ee349e0a209a46ce720c111ed870cd6932a
Chris Kuehl <ckuehl@yelp.com>
2017-04-27 22:25:21 -0700

    Release v0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='lazy-build',
-    version='0.2.0',
+    version='0.3.0',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
